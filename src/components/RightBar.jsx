0d9ec57d23b7d2cadc3611239871d72accf10da9
Umang Saxena <hanusaxena68@gmail.com>
2023-09-18 11:56:15 +0530

    Right bar Made

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -1,17 +1,197 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Divider,
+  ImageList,
+  ImageListItem,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const RightBar = () => {
   return (
-    <Box
-      bgcolor="yellow"
-      flex={2}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
+    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
       <Box position="fixed">
+        {/* Avatar Sections */}
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup width={200} max={7}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://mui.com/static/images/avatar/1.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://mui.com/static/images/avatar/2.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://mui.com/static/images/avatar/3.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://mui.com/static/images/avatar/5.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+          <Avatar
+            alt=" Henderson"
+            src="https://mui.com/static/images/avatar/5.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+          <Avatar
+            alt="Mathews Henderson"
+            src="https://mui.com/static/images/avatar/5.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+          <Avatar
+            alt="Wilia Henderson"
+            src="https://mui.com/static/images/avatar/5.jpg"
+            sx={{ width: 50, height: 50 }}
+          />
+        </AvatarGroup>
+        
+        {/* Photos Section */}
+        <Typography variant="h6" fontWeight={100} margin={2}>
+          Recent Photos
+        </Typography>
+        <ImageList cols={3} gap={4} rowHeight={130} sx={{"overflow-y":"hidden"}} >
+          <ImageListItem>
+            <img src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=164&h=164&fit=crop&auto=format" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=164&h=164&fit=crop&auto=format" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=164&h=164&fit=crop&auto=format" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=164&h=164&fit=crop&auto=format" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=164&h=164&fit=crop&auto=format" />
+          </ImageListItem>
+          <ImageListItem>
+            <img src="https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=164&h=164&fit=crop&auto=format" />
+          </ImageListItem>
+        </ImageList>
 
-      RightBar
+
+        {/* Chats Section */}
+        <Typography variant="h6" fontWeight={100} margin={2}>
+          Chats
+        </Typography>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper"}}
+        >
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Remy Sharp"
+                src="https://mui.com/static/images/avatar/1.jpg"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Travis Howard"
+                src="https://mui.com/static/images/avatar/2.jpg"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Cindy Baker"
+                src="https://mui.com/static/images/avatar/3.jpg"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
       </Box>
     </Box>
   );
